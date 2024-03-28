0f0bae4b0b10e1140c4a044940cb88878d39d429
Mike Hunsicker <hunsickm@matc.edu>
2024-03-28 11:37:19 -0500

    Used Main and Footer components

--- blog/src/components/layout.js
+++ blog/src/components/layout.js
@@ -6,6 +6,8 @@ import styled, { ThemeProvider } from "styled-components"
 import { Header } from "./Header"
 import "./layout.css"
 import { Gray } from "./themes/Gray"
+import { Main } from './Main'
+import { Footer } from './Footer'
 
 const Content = styled.div`
   margin: 0 auto;
@@ -28,8 +30,8 @@ const Layout = ({ children }) => {
       <ThemeProvider theme={Gray}>
         <Header siteTitle={data.site.siteMetadata?.title || `Title`} />
         <Content>
-          <main>{children}</main>
-          <footer
+          <Main>{children}</Main>
+          <Footer
             style={{
               marginTop: `var(--space-5)`,
               fontSize: `var(--font-sm)`,
@@ -38,7 +40,7 @@ const Layout = ({ children }) => {
             © {new Date().getFullYear()} &middot; Built with
             {` `}
             <a href="https://www.gatsbyjs.com">Gatsby</a>
-          </footer>
+          </Footer>
         </Content>
       </ThemeProvider>
   )
